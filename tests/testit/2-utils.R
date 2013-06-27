a154ad5b56a2c7ee45de6b95a60294fde590d548
Yihui Xie <xie@yihui.name>
2013-06-27 00:00:00 +0000

    version 0.1

---
+++ tests/testit/2-utils.R
@@ -0,0 +1,9 @@
+library(testit)
+
+# no need to use testit:::available_dir()
+assert(
+  'available_dir() should find an existing directory',
+  file.exists(
+    available_dir(c('foobar', 'whatever', '~', system.file('man', package = 'testit')))
+  )
+)
